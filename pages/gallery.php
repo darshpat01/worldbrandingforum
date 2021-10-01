247455faf249da650c9fb2393374b3966d412f7d
darshpat01 <darshpat01@gmail.com>
2021-10-01 11:58:26 +0530

    added gallery page

---
+++ pages/gallery.php
@@ -0,0 +1,37 @@
+<?php /* Template Name: Gallery */
+
+get_header(); ?>
+<section class="wbf-gallery">
+    <div class="container">
+        <h2 class="wbf-gallery-head">WORLD BRANDING AWARDS – KENSINGTON PALACE 2019</h2>
+        <div class="wbf-gallery-images">
+            <a href=""> <img src="https://picsum.photos/150/130" alt=""></a>
+            <a href=""> <img src="https://picsum.photos/150/130" alt=""></a>
+            <a href=""> <img src="https://picsum.photos/150/130" alt=""></a>
+            <a href=""> <img src="https://picsum.photos/150/130" alt=""></a>
+            <a href=""> <img src="https://picsum.photos/150/130" alt=""></a>
+            <a href=""> <img src="https://picsum.photos/150/130" alt=""></a>
+            <a href=""> <img src="https://picsum.photos/150/130" alt=""></a>
+            <a href=""> <img src="https://picsum.photos/150/130" alt=""></a>
+            <a href=""> <img src="https://picsum.photos/150/130" alt=""></a>
+            <a href=""> <img src="https://picsum.photos/150/130" alt=""></a>
+            <a href=""> <img src="https://picsum.photos/150/130" alt=""></a>
+            <a href=""> <img src="https://picsum.photos/150/130" alt=""></a>
+            <a href=""> <img src="https://picsum.photos/150/130" alt=""></a>
+            <a href=""> <img src="https://picsum.photos/150/130" alt=""></a>
+            <a href=""> <img src="https://picsum.photos/150/130" alt=""></a>
+            <a href=""> <img src="https://picsum.photos/150/130" alt=""></a>
+            <a href=""> <img src="https://picsum.photos/150/130" alt=""></a>
+            <a href=""> <img src="https://picsum.photos/150/130" alt=""></a>
+            <a href=""> <img src="https://picsum.photos/150/130" alt=""></a>
+            <a href=""> <img src="https://picsum.photos/150/130" alt=""></a>
+            <a href=""> <img src="https://picsum.photos/150/130" alt=""></a>
+
+
+        </div>
+    </div>
+
+</section>
+
+
+<?php get_footer();
