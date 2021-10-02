cf14ef7b832833605cd7c42c9a2c09ba2956a864
darshpat01 <darshpat01@gmail.com>
2021-10-02 16:34:59 +0530

    completed videos page

--- pages/video.php
+++ pages/video.php
@@ -1,6 +1,7 @@
 <?php /* Template Name: Video */
 
 get_header(); ?>
+<!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> -->
 <section class="wbf-video">
     <div class="container">
         <h2 class="wbf-video-head">HIGHLIGHT VIDEOS</h2>
@@ -24,9 +25,59 @@ get_header(); ?>
 
                 </div>
             </div>
+            <div class="row wbf-3-vids">
+                <div class="col-12 col-md-4 wbf-iframe-3">
+                    <h2 class="wbf-video-head2">KENSINGTON PALACE 2019</h2>
+                    <div class="wbf-iframediv">
+                        <iframe class="wbf-video-iframe" src="https://www.youtube.com/embed/soTFncL4f7E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <!-- <iframe title="World Branding Awards 2019 - Kensington Palace, London" src="https://www.youtube.com/embed/tgbNymZ7vqY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe> -->
+                    </div>
+                </div>
+                <div class="col-12 col-md-4 wbf-iframe-3">
+                    <h2 class="wbf-video-head2">KENSINGTON PALACE 2019</h2>
+                    <div class="wbf-iframediv">
+                        <iframe class="wbf-video-iframe" src="https://www.youtube.com/embed/soTFncL4f7E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <!-- <iframe title="World Branding Awards 2019 - Kensington Palace, London" src="https://www.youtube.com/embed/tgbNymZ7vqY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe> -->
+                    </div>
+                </div>
+                <div class="col-12 col-md-4 wbf-iframe-3">
+                    <h2 class="wbf-video-head2">KENSINGTON PALACE 2019</h2>
+                    <div class="wbf-iframediv">
+                        <iframe class="wbf-video-iframe" src="https://www.youtube.com/embed/soTFncL4f7E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <!-- <iframe title="World Branding Awards 2019 - Kensington Palace, London" src="https://www.youtube.com/embed/tgbNymZ7vqY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe> -->
+                    </div>
+                </div>
+
+                <div class="col-12 col-md-4 wbf-iframe-3">
+                    <h2 class="wbf-video-head2">KENSINGTON PALACE 2019</h2>
+                    <div class="wbf-iframediv">
+                        <iframe class="wbf-video-iframe" src="https://www.youtube.com/embed/soTFncL4f7E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <!-- <iframe title="World Branding Awards 2019 - Kensington Palace, London" src="https://www.youtube.com/embed/tgbNymZ7vqY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe> -->
+                    </div>
+                </div>
+                <div class="col-12 col-md-4 wbf-iframe-3">
+                    <h2 class="wbf-video-head2">KENSINGTON PALACE 2019</h2>
+                    <div class="wbf-iframediv">
+                        <iframe class="wbf-video-iframe" src="https://www.youtube.com/embed/soTFncL4f7E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <!-- <iframe title="World Branding Awards 2019 - Kensington Palace, London" src="https://www.youtube.com/embed/tgbNymZ7vqY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe> -->
+                    </div>
+                </div>
+                <div class="col-12 col-md-4 wbf-iframe-3">
+                    <h2 class="wbf-video-head2">KENSINGTON PALACE 2019</h2>
+                    <div class="wbf-iframediv">
+                        <iframe class="wbf-video-iframe" src="https://www.youtube.com/embed/soTFncL4f7E" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                        <!-- <iframe title="World Branding Awards 2019 - Kensington Palace, London" src="https://www.youtube.com/embed/tgbNymZ7vqY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen=""></iframe> -->
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
-
+    <div class="wbf-visit-yt">
+        <div class="container wbf-yt-cont">
+            <h2>Visit our YouTube Channel for more videos</h2>
+            <a class="btn btn-outline-primary">YouTube</a>
+        </div>
+    </div>
 </section>
 
 
