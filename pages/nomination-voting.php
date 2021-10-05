2fd516f4b3720e5b53eddaa0b7f4b2fd83154836
darshpat01 <darshpat01@gmail.com>
2021-10-06 03:12:52 +0530

    started nominations page

---
+++ pages/nomination-voting.php
@@ -0,0 +1,23 @@
+<?php /* Template Name: nominations */
+
+get_header(); ?>
+<section class="wbf-trophy">
+    <div class="wbf-trophy-header">
+        <div class="container wbf-trophy-head-cont">
+            <h2>Nomination & Voting</h2>
+        </div>
+    </div>
+    <div class="wbf-presenters-cont container">
+        <h2>The World Branding Awards has worked with
+            <br>
+            stars and celebrities since the very beginning
+        </h2>
+        <hr class="wbf-hr">
+        <h3>THE NOMINATION AND VOTING PLATFORM FOR BOTH THE GLOBAL AND AMERICAS EDITIONS OF THE WORLD BRANDING AWARDS ARE NOW OPEN.</h3>
+        <a class="btn btn-outline-primary">Vote Now</a>
+
+    </div>
+</section>
+
+
+<?php get_footer();
