62a8758364b9d7fe2c4194a5a9b290e28547c15f
darshpat01 <darshpat01@gmail.com>
2021-10-07 04:22:23 +0530

    added enter vote and judging page

---
+++ pages/judging.php
@@ -0,0 +1,48 @@
+<?php /* Template Name: judging */
+
+get_header(); ?>
+<section class="wbf-trophy">
+    <div class="wbf-trophy-header">
+        <div class="container wbf-trophy-head-cont">
+            <h2>JUDGING & VOTING</h2>
+        </div>
+    </div>
+    <div class="wbf-presenters-cont container">
+        <div class="wbf-judging-logo">
+            <img src="https://awards.brandingforum.org/wp-content/uploads/2014/03/WBA-BOTY-R-Logo.png" alt="">
+        </div>
+        <br>
+        <h2>THE JUDGING AND VOTING PROCESSES OF THE WORLD BRANDING AWARDS ARE UNIQUE AS POINTS ARE AWARDED THROUGH THREE STREAMS, WITH EACH STREAM CONTRIBUTING A SET PERCENTAGE TO THE TOTAL SCORE IN EVALUATING A BRAND.
+        </h2>
+        <hr>
+        <div class="wbf-judging-eval">
+            <div class="wbf-eval-i">
+                <h2>BRAND VALUATION – 30%</h2>
+                <p>The World Branding Forum’s research department conducts a financial valuation of a brand based on an income approach model, which also takes into account a projection period. For public companies, data is gathered from available published sources, while for non-public companies, audited financial data is provided by the companies for evaluation or from publicly available sources.</p>
+                <hr>
+            </div>
+            <div class="wbf-eval-i">
+                <h2>PUBLIC ONLINE VOTING – 30%</h2>
+                <p>The World Branding Forum runs an ongoing online nomination and voting platform designed specifically for online research. Although this may be considered as a populist or peoples’ choice vote, the scores for this stream only contributes to one part of the judging process, so the popularity aspect of a brand alone may not result in a brand winning</p>
+                <hr>
+            </div>
+            <div class="wbf-eval-i">
+                <h2>CONSUMER MARKET RESEARCH – 40%</h2>
+                <p>The World Branding Forum’s research centre conduct targeted consumer market research in all countries that are selected for the awards. The results from this survey contribute to the market research score for that brand both globally and also for a particular country.</p>
+                <hr>
+            </div>
+        </div>
+        <h2>ADVISORY COUNCILS</h2>
+        <p>Advisory Councils may be setup from time to time. Council members are involved with the nomination and voting process for special awards. </p>
+        <hr>
+        <p>
+        <div class="row wbf-judging-btns">
+            <div class="col-6"><a class="btn btn-outline-primary" href="">Vote for your favourite brand!</a></div>
+            <div class="col-6"><a class="btn btn-outline-primary" href="">Brands & Agencies Nomination Process</a></div>
+        </div>
+        </p>
+    </div>
+</section>
+
+
+<?php get_footer();
--- pages/nomination-voting.php
+++ pages/nomination-voting.php
@@ -8,14 +8,25 @@ get_header(); ?>
         </div>
     </div>
     <div class="wbf-presenters-cont container">
-        <h2>The World Branding Awards has worked with
-            <br>
-            stars and celebrities since the very beginning
-        </h2>
+        <h1>Branding Entries & Public Voting </h1>
         <hr class="wbf-hr">
         <h3>THE NOMINATION AND VOTING PLATFORM FOR BOTH THE GLOBAL AND AMERICAS EDITIONS OF THE WORLD BRANDING AWARDS ARE NOW OPEN.</h3>
         <a class="btn btn-outline-primary">Vote Now</a>
-
+        <br>
+        <br>
+        <p>The next World Branding Awards ceremony will be the Animalis Edition, will take place in July 2021 at Hofburg Palace – Vienna, and the Global Awards will return to the home of the World Branding Forum at Kensington Palace, London in the Autumn of 2021.</p>
+        <hr class="wbf-hr">
+        <h3>If you are an agency or brand owner and would like to submit your brand for the World Branding Awards, please click the button below.</h3>
+        <a class="btn btn-outline-primary">Start Brand Submission</a>
+        <br>
+        <br>
+        <p>Entry nomination and registration requires providing detailed information about the brand being nominated.</p>
+        <br>
+        <p>There is an administrative fee of £180 per entry</p>
+        <p>Excludes VAT for UK and non-VAT registered EU brands.</p>
+        <hr class="wbf-hr">
+        <p>Already registered? <a href="">Login Here</a></p>
+        <p><a href="">Click here for the Cancellation Policy for Participating Brands</a></p>
     </div>
 </section>
 
